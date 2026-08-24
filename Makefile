2514e3f8784277316ad9ec09751763f1f818fcc6
l-m <l-m@l-m.dev>
2026-08-24 10:28:43 +1000

    build: init mrrrp

---
+++ Makefile
@@ -0,0 +1,4 @@
+.PHONY: serve
+serve:
+	@echo http://localhost:8080
+	air
---
+++ webring.js
@@ -0,0 +1,12 @@
+// https://nginx.org/en/docs/njs
+// https://github.com/nginx/njs-examples
+
+export function wrnext(r) {
+    // TODO
+    r.return(302, '/');
+}
+
+export function wrprev(r) {
+    // TODO
+    r.return(302, '/');
+}
